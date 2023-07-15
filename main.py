79252cc769e170a8cdd5c2aa0f270a61c6febf0d
lubnabts <lydalala17@eng.just.edu.jo>
2023-07-15 01:49:37 -0700

    up

--- main.py
+++ main.py
@@ -29,3 +29,49 @@
 # var = "hellow"
 # var = var [:2]+ "mm"+var [4:]
 # print (var)
+
+
+# if 7 ==8:
+#     print ("True1")
+# elif 9 ==9 :
+#     print ("True2")
+# else:
+#     print ("NOTHING")
+
+
+# switch :
+#     case 7 ==8:
+#         print ("True1")
+#     case 9 ==9 :
+#         print ("True2")
+#     defult:
+#          print ("NOTHING")
+
+# num =0
+# while num < 10:
+#     print (num ) 
+#     num =num +1
+
+# vv= "noor"
+# user = input ("enter word : ")
+# c = 1
+# while c < 2:
+#     if vv ==user:
+#         print ("True")
+#     else :
+#         user = input ("enter word again : ")
+#         if vv ==user:
+#              print ("True")
+#         else :
+#             print("false")
+#     c=c+1
+
+# c = 0
+# while c <5:
+#     print ('-'*50)
+#     user = input ("enter your string ")
+#     if user == 'l'
+   
+
+
+
